6c9069ebe4fd54539e714f356aef1462c1a5b537
RyanCarson3228 <rcarson95@gmail.com>
2018-03-15 10:12:55 -0400

    second commit

---
+++ src/main/java/org/wecancodeit/columbus/restfulcities/RestfulCitiesApplication.java
@@ -0,0 +1,12 @@
+package org.wecancodeit.columbus.restfulcities;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulCitiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulCitiesApplication.class, args);
+	}
+}
